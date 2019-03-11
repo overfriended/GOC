03321f3e80ccf0b5de05efce710929624d11cba0
VRPCaden <giantballofcrap@gmail.com>
2019-03-10 21:33:15 -0300

    Base?

--- GOC/GOC.cs
+++ GOC/GOC.cs
@@ -9,6 +9,9 @@ namespace GOC
 {
     public class GOC : Plugin
     {
+
+        public static Random rand = new Random();
+
         public override void OnDisable()
         {
 
@@ -21,7 +24,17 @@ namespace GOC
 
         public override void Register()
         {
-
+            this.AddConfig(new Smod2.Config.ConfigSetting("goc_chance", 50, Smod2.Config.SettingType.NUMERIC, true, "Percent chance for the GOC to spawn."));
+            this.AddConfig(new Smod2.Config.ConfigSetting("goc_hp", 200, Smod2.Config.SettingType.NUMERIC, true, "The HP for the GOC."));
+            this.AddConfig(new Smod2.Config.ConfigSetting("goc_items", new[]
+            {
+                20,
+                25,
+                12,
+                16,
+                8,
+                14
+            }, Smod2.Config.SettingType.NUMERIC_LIST, true, "The items they get."));
         }
     }
 }
---
+++ GOC/RoundEvents.cs
@@ -0,0 +1,28 @@
+﻿using Smod2.API;
+using Smod2.EventHandlers;
+using Smod2.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOC
+{
+    class RoundEvents : IEventHandlerRoundStart
+    {
+
+        int index = GOC.rand.Next(0, 101);
+
+        public void OnRoundStart(RoundStartEvent ev)
+        {
+            foreach (Player p in ev.Server.GetPlayers())
+            {
+                if (p.TeamRole.Role == Role.FACILITY_GUARD && index >                                                                                                                                                                                                                                                                                                                                                                                                                                                   )
+                {
+                    p.ChangeRole(Role.NTF_COMMANDER);
+                }
+            }
+        }
+    }
+}
