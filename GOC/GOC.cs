dace3a97cba6b484d07fdb326df823bf09b41689
VRPCaden <giantballofcrap@gmail.com>
2019-03-10 21:01:01 -0300

    Init

---
+++ GOC/GOC.cs
@@ -0,0 +1,27 @@
+﻿using Smod2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOC
+{
+    public class GOC : Plugin
+    {
+        public override void OnDisable()
+        {
+
+        }
+
+        public override void OnEnable()
+        {
+
+        }
+
+        public override void Register()
+        {
+
+        }
+    }
+}
